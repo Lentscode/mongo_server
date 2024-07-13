af7ca287757c99ee747e35148da7744620fe9f29
Lentscode <antonio.lentini2004@gmail.com>
2024-07-13 11:04:20 +0200

    Update

--- lib/routes/purchase_product.dart
+++ lib/routes/purchase_product.dart
@@ -1,12 +1,21 @@
 part of "routes.dart";
 
+// Handler per aggiornare la quantità disponibile di un prodotto
+// in caso venga acquistato.
 Future<Response> purchaseProduct(Request req) async {
   try {
     final data = await RequestUtils.getPayload(req);
 
-    final id = data["_id"] as String;
-    final amount = data["amount"] as int;
+    // Accediamo ai dati inviati dal client.
+    final id = data["_id"];
+    final amount = data["amount"];
 
+    // Controlliamo che non siano nulli.
+    if (id == null || amount == null) {
+      return Response.badRequest(body: "Id or amount missing");
+    }
+
+    // Aggiorniamo il documento del prodotto e lo recuperiamo.
     final product = await getIt.get<ProductRepository>().updateAmountOfProduct(id, amount);
 
     return Response.ok(
@@ -14,6 +23,8 @@ Future<Response> purchaseProduct(Request req) async {
       headers: {"Content-Type": "application/json"},
     );
   } on ProductNotFoundException catch (e) {
+    // In caso il prodotto non venga trovato, viene lanciata un'eccezione,
+    // che intercettiamo.
     return Response.badRequest(body: "Product with id: ${e.productId} not found");
   }
 }
