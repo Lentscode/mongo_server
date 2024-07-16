29180b1c152390f88616c5c1aaa4f8ffabf3c73b
Lentscode <antonio.lentini2004@gmail.com>
2024-07-16 11:31:08 +0200

    Update

--- lib/config/set_up.dart
+++ lib/config/set_up.dart
@@ -2,8 +2,6 @@ import "package:dotenv/dotenv.dart";
 import "package:get_it/get_it.dart";
 import "package:mongo_dart/mongo_dart.dart";
 
-import "../utils/utils.dart";
-
 // Variabile globale che contiene tutte le classi ausiliarie.
 final getIt = GetIt.instance;
 
@@ -14,11 +12,4 @@ Future<void> setUp() async {
   // Creiamo un'istanza al database MongoDB e apriamo la connessione.
   final db = await Db.create(env["MONGO_CREDENTIALS"]!);
   await db.open();
-
-  // Creiamo un'oggetto [ProductRepository] passandogli la referenza alla collezione
-  // dei prodotti in MongoDB.
-  final productRepository = ProductRepository(db.collection("products"));
-
-  // Registriamo la nostra repository nel service locator.
-  getIt.registerSingleton<ProductRepository>(productRepository);
 }
