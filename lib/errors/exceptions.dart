fe07c2e7ccb6e7c20ecbe716cd779d9ee9606bd4
Lentscode <antonio.lentini2004@gmail.com>
2024-08-05 22:34:16 +0200

    :goal_net: Added exception

--- lib/errors/exceptions.dart
+++ lib/errors/exceptions.dart
@@ -1,6 +1,16 @@
+import "package:mongo_dart/mongo_dart.dart";
+
 // Eccezione lanciata in caso di prodotto non trovato.
 class ProductNotFoundException {
   ProductNotFoundException(this.productId);
 
-  final String productId;
+  final ObjectId productId;
+}
+
+class InsufficientAmountException {
+  const InsufficientAmountException(this.productId, this.requested, this.available);
+
+  final ObjectId productId;
+  final int requested;
+  final int available;
 }
